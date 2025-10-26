7311a12dfefb47b62f958d1ba7c776b8e48dcaae
craigenders <cenders@ucla.edu>
2025-10-26 16:10:22 -0700

    Update functions.R

--- misc/functions.R
+++ misc/functions.R
@@ -13,29 +13,31 @@ plot_imputations <- function(model, raw_df, var, bins = 50, main = NULL) {
   imps_list <- model@imputations
   imp_vals <- unlist(lapply(imps_list, function(d) d[[var]][miss_idx]), use.names = FALSE)
   obs_vals <- raw_df[[var]][!miss_idx]
-  
   if (!is.numeric(obs_vals) || !is.numeric(imp_vals))
     stop("Variable must be numeric to plot histogram")
   
   rng <- range(c(obs_vals, imp_vals), na.rm = TRUE)
   if (is.null(main)) main <- paste("Observed vs Imputed Data:", var)
-  
-  # Compute consistent breaks
   breaks <- seq(rng[1], rng[2], length.out = bins + 1)
   
-  # Plot observed histogram (filled grey)
+  # Observed (filled grey)
   hist(obs_vals, breaks = breaks, freq = FALSE,
        col = "grey70", border = "grey70",
        xlim = rng, main = main, xlab = var)
   
-  # Add imputed histogram outline (black)
+  # Imputed (blue outline)
   hist(imp_vals, breaks = breaks, freq = FALSE,
        add = TRUE, col = NA, border = "blue", lwd = 2)
   
+  par(xpd = NA)  # allow drawing into figure region, not clipped by plot
   legend("topright",
+         inset = c(-0.02, -0.02),   # small negative inset to push outward
          legend = c("Observed", "Imputed"),
          fill   = c("grey70", NA),
          border = c("grey70", "blue"),
-         bty = "n",
-         lwd = NA)  # suppress line drawing
+         bty    = "n",
+         lty    = 0, lwd = 0,
+         text.col = "black",
+         xpd = NA)
+  par(xpd = FALSE)
 }
