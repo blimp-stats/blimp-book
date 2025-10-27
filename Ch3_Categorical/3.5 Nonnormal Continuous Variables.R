89e228c2286e05efd2bb984c8cda3218973aeca3
craigenders <cenders@ucla.edu>
2025-10-27 06:58:55 -0700

    Update

--- Ch3_Categorical/3.5 Nonnormal Continuous Variables.R
+++ Ch3_Categorical/3.5 Nonnormal Continuous Variables.R
@@ -13,17 +13,35 @@ data_url <- 'https://raw.githubusercontent.com/blimp-stats/blimp-book/main/data/
 # create data frame from github data
 inflamm <- read.csv(data_url)
 
+# plotting function
+source('https://raw.githubusercontent.com/blimp-stats/blimp-book/main/misc/functions.R')
+
 # SUMMARIZE DATA ----
 
 describe(inflamm)
-
 hist(inflamm$dpdd, breaks = 50)
 hist(inflamm$inflam_sum, breaks = 50)
 
 # FIT MODEL ----
 
-# yeo-johnson transformation for nonnormal outcome
+# nonnormal outcome
 model1 <- rblimp(
+  data = inflamm,
+  nominal = 'female els',
+  center = 'age',
+  # fixed = 'male',
+  model = 'dpdd ~ female els age inflam_sum', 
+  seed = 90291,
+  burn = 10000,
+  iter = 10000,
+  nimps = 20)
+
+output(model1)
+
+plot_imputations_overlay(model1, inflamm, var = "dpdd")
+
+# yeo-johnson transformation for nonnormal outcome
+model2 <- rblimp(
   data = inflamm,
   nominal = 'female els',
   center = 'age',
@@ -34,5 +52,5 @@ model1 <- rblimp(
   iter = 10000,
   nimps = 20)
 
-output(model1)
-
+plot_imputations_overlay(model2, inflamm, var = "dpdd")
+plot_imputations(model2, var = "yjt.dpdd.6.")
