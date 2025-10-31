46f34da7c9d0a22eeaebc4389573db9af40ff8a5
craigenders <cenders@ucla.edu>
2025-10-30 18:43:29 -0700

    Update 7.4 Mediation w Discrete Variables.R

--- Ch7_Mediation/7.4 Mediation w Discrete Variables.R
+++ Ch7_Mediation/7.4 Mediation w Discrete Variables.R
@@ -1,4 +1,4 @@
-# NEGATIVE BINOMIAL REGRESSION FOR A COUNT OUTCOME ----
+# MEDIATION WITH DISCRETE INDICATORS ----
 
 # LOAD R PACKAGES ----
 
@@ -12,17 +12,90 @@ data_url <- 'https://raw.githubusercontent.com/blimp-stats/blimp-book/main/data/
 # create data frame from github data
 alcoholuse <- read.csv(data_url)
 
-# FIT MODEL ----
+# FIT MEDIATION MODEL WITH DISCRETE MEDIATOR ----
 
-# negative binomial regression
-model <- rblimp(
+# mediation with a binary logistic outcome
+model_lm <- rblimp(
+  data = alcoholuse,
+  nominal = 'college male alcearly', 
+  center  = 'college age',
+  # fixed = 'male',
+  model = '
+    apath:
+    alcearly ~ intercept@m_icept male@a college age;
+    bpath:
+    alcdays ~ intercept@y_icept alcage@b male@tau college age',
+  parameters = '
+    ind_female = a * b * exp( m_icept + a*0 ) /
+       ( 1 + exp( m_icept + a*0 ) )^2;
+    ind_male = a * b * exp( m_icept + a*1 ) /
+     ( 1 + exp( m_icept + a*1 ) )^2;
+    ind_diff   = ind_male - ind_female;',
+  seed = 90291,
+  burn = 10000,
+  iter = 10000)
+
+output(model_lm)
+
+# plot distribution of indirect effects
+posterior_plot(model_lm, 'ind_male')
+posterior_plot(model_lm, 'ind_female')
+posterior_plot(model_lm, 'ind_diff')
+
+# FIT MEDIATION MODEL WITH DISCRETE OUTCOMES ----
+
+# mediation with a binary logistic outcome
+model_lo <- rblimp(
+  data = alcoholuse,
+  nominal = 'college male drinker', 
+  center  = 'college age',
+  # fixed = 'male',
+  model = '
+    apath:
+    alcage ~ intercept@m_icept male@a college age;
+    bpath:
+    drinker ~ intercept@y_icept alcage@b male@tau college age',
+  parameters = '
+    ind_female = a * ( b * exp( y_icept + b*(m_icept + a*0) + tau*0 ) ) /
+      ( 1 + exp( y_icept + b*(m_icept + a*0) + tau*0 ) )^2;
+    ind_male   = a * ( b * exp( y_icept + b*(m_icept + a*1) + tau*1 ) ) /
+      ( 1 + exp( y_icept + b*(m_icept + a*1) + tau*1 ) )^2;
+    ind_diff = ind_male - ind_female;',
+  seed = 90291,
+  burn = 10000,
+  iter = 10000)
+
+output(model_lo)
+
+# plot distribution of indirect effects
+posterior_plot(model_lo, 'ind_male')
+posterior_plot(model_lo, 'ind_female')
+posterior_plot(model_lo, 'ind_diff')
+
+# mediation with a count outcome
+model_co <- rblimp(
     data = alcoholuse,
     nominal = 'college male',
     count = 'alcdays',
+    center = 'college age',
     # fixed = 'male',
-    model = 'alcdays ~ alcage college age male', 
+    model = '
+      apath:
+      alcage ~ intercept@m_icept male@a college age;
+      bpath:
+      alcdays ~ intercept@y_icept alcage@b male@tau college age',
+    parameters = '
+      ind_female = a * (b*exp(y_icept + b*m_icept + tau*0)); 
+      ind_male = a * (b*exp(y_icept + b*(m_icept + a*1) + tau*1));
+      ind_diff = ind_female - ind_male;',
     seed = 90291,
     burn = 10000,
     iter = 10000)
 
-output(model)
+output(model_co)
+
+# plot distribution of indirect effects
+posterior_plot(model_co, 'ind_male')
+posterior_plot(model_co, 'ind_female')
+posterior_plot(model_co, 'ind_diff')
+
