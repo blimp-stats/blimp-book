8bacd31df8c3a5d3c01374771f13595d5addabec
craigenders <craig.enders@gmail.com>
2026-07-24 14:10:44 -0700

    Updates

--- Ch7_Multilevel/7.4 MLM w Interaction.R
+++ Ch7_Multilevel/7.4 MLM w Interaction.R
@@ -26,7 +26,7 @@ trial <- read.csv(data_url)
 # PLOT MEANS ----
 #------------------------------------------------------------------------------#
 
-ggplot(trial, aes(x = visit, y = severity)) +
+ggplot(trial, aes(x = visit, y = severity, color = drug, group = drug)) +
   stat_summary(fun = mean, geom = "line",  na.rm = TRUE) +
   stat_summary(fun = mean, geom = "point", na.rm = TRUE)
 
@@ -48,7 +48,33 @@ output(mod1)
 # plot parameter distributions
 posterior_plot(mod1)
 
+#------------------------------------------------------------------------------#
+# LINEAR GROWTH MODEL WITH PREDICTORS ----
+#------------------------------------------------------------------------------#
 
+mod2 <- rblimp(
+  data = trial,
+  ordinal = 'male drug',
+  clusterid = 'person',
+  center = 'grandmean = male',
+  model = 'severity ~ intercept@icept_d0 visit@slp_d0 drug@icept_diff drug*visit@slp_diff male | visit',
+  simple = 'visit | drug',
+  parameters = '
+    mu3_drug0 = icept_d0 + slp_d0*3;
+    mu3_drug1 = (icept_d0 + icept_diff) + (slp_d0 + slp_diff)*3;
+    mu3_diff = mu3_drug1 - mu3_drug0;',
+  seed = 90291,
+  burn = 10000,
+  iter = 10000)
+
+# print output
+output(mod2)
+
+# plot parameter distributions
+posterior_plot(mod2)
+
+# plot conditional growth curves
+simple_plot(severity ~ visit | drug, mod2)
 
 #------------------------------------------------------------------------------#
 # GRAPHICAL DIAGNOSTICS WITH MULTIPLE IMPUTATIONS ----
@@ -59,10 +85,11 @@ posterior_plot(mod1)
 
 
 #------------------------------------------------------------------------------#
-# LINEAR GROWTH MODEL (LATENT SPECIFICATION) ----
+# FIT LINEAR GROWTH MODEL (LATENT SPECIFICATION) ----
 #------------------------------------------------------------------------------#
 
-modelx <- rblimp(
+# linear growth model
+model2 <- rblimp(
   data = trial,
   clusterid = 'person',
   latent = 'person = icept linear',
@@ -78,7 +105,32 @@ modelx <- rblimp(
   nimps = 20)
 
 # print output
-output(modelx)
+output(model2)
+
+# linear growth model with predictors
+model4 <- rblimp(
+  data = trial,
+  ordinal = 'male drug',
+  clusterid = 'person',
+  center = 'grandmean = male',
+  latent = 'person = icept linear;',
+  model = '
+    level2:
+    icept ~ intercept@icept_d0 drug@icept_diff male;
+    linear ~ intercept@slp_d0 drug@slp_diff;
+    icept ~~ linear;
+    level1:
+    severity ~ intercept@icept visit@linear;',
+  parameters = '
+    mu3_drug0 = icept_d0 + slp_d0*3;
+    mu3_drug1 = (icept_d0 + icept_diff) + (slp_d0 + slp_diff)*3;
+    mu3_diff = mu3_drug1 - mu3_drug0;',
+  seed = 90291,
+  burn = 10000,
+  iter = 10000)
+
+# print output
+output(model4)
 
 #------------------------------------------------------------------------------#
 # BOOK FIGURE THEME ----
